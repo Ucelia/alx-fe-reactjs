3b0bef56b303f34368c0d78943b08e430fc8da27
celia <neevecelia@gmail.com>
2024-11-16 16:37:46 +0200

    added the alx-react-app-new folder

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+
+    return(
+        <footer style={{opacity: '0.4'}}>
+        <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+export default function MainContent() {
+
+    return(
+        <main style={{fontSize: '30px'}}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
