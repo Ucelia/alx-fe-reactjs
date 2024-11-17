255b1c390cc2b6926bc56a78d6948e1f94f5d27e
celia <neevecelia@gmail.com>
2024-11-17 23:31:53 +0200

    added my-company folder

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer style={{ padding: '20px', textAlign: 'center', backgroundColor: '#f1f1f1' }}>
+          <p>&copy; 2024 My Company. All Rights Reserved.</p>
+        </footer>
+      );
+    }
