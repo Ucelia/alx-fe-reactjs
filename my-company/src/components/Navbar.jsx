62a7ec9b3a7f38d7f8b656ade9cd451279f08836
celia <neevecelia@gmail.com>
2024-11-18 00:00:39 +0200

    changes

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
       <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
         <Link to="/" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Home</Link>
         <Link to="/about" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>About</Link>
-        <Link to="/services" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Services</Link>
+        <Link to="/services" style={{ margin: '0 10px', color: '#fff', dislay:'', justifyContent:'', textDecoration: 'none' }}>Services</Link>
         <Link to="/contact" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Contact</Link>
       </nav>
     );
