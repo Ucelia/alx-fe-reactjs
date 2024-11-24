46879976af1059cfa18c7a6d8d062bfe5cc8f640
celia <neevecelia@gmail.com>
2024-11-24 19:41:31 +0200

    Implemented navigation

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate, useNavigate } from 'react-router-dom';
 import useRecipeStore from './recipeStore'
 
 const DeleteRecipeButton = ({recipeId}) =>{
     const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+    const useNavigate = useNavigate();
 
     const handleDelete = ()=> {
         deleteRecipe(recipeId);
+        Navigate('/');
     };
 
     return 
