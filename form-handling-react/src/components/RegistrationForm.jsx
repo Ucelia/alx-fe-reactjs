ed90305a77e7ff52a90806f2e5cc496a5ceeef89
celia <neevecelia@gmail.com>
2024-11-27 15:07:57 +0200

    defined the variables separately

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 
 const RegistrationForm = () => {
-    const [formData, setFormData] = useState({username:'', email:'',password:''});
+    // const [formData, setFormData] = useState({username:'', email:'',password:''});
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -14,25 +18,25 @@ const RegistrationForm = () => {
     };
 
     return(
-        <form onSubmit={{handleSubmit}}>
+        <form onSubmit={handleSubmit}>
             <label>User Name: </label>
             <input 
             type="text"
             name="username"
-            value={formData.username}
+            value={username}
             onChange={handleChange} required/><br/>
 
             <label>Email: </label>
             <input 
             type="email" 
             name="email"
-            value={formData.email}
+            value={email}
             onChange={handleChange} required /><br/>
 
             <label>Password: </label>
             <input type="password"
             name="password"
-            value={formData.password}
+            value={password}
             onChange={handleChange} required/><br/><br/>
 
             <button type="submit">Submit</button>
