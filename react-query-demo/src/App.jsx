c381fc665f63d42ced7f58281de3a5d0c1735ab0
celia <neevecelia@gmail.com>
2024-11-30 01:42:59 +0200

    changes

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <QueryClientProvider client={QueryClient}>
+    <QueryClientProvider client={queryClient}>
         <PostsComponent />
       </QueryClientProvider>
   );
