222ca1b10691cd8207b8426e770dce40768abdc6
celia <neevecelia@gmail.com>
2024-11-30 01:41:51 +0200

    Added react-query-demo folder

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <QueryClientProvider client={QueryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
+  );
+}
+
+export default App
