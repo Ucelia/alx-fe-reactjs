e5655b2bd03465155fddb9034440d83ce6efc41a
celia <neevecelia@gmail.com>
2024-12-07 15:37:09 +0200

    added more interactivity

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,11 +1,12 @@
 function UserProfile() {
     return (
       <div className="user-profile bg-gray-100 sm:p-4 md:p-8 
-      mx-auto my-20 rounded-lg shadow-lg sm:max-w-xs md:max-w-sm">
+      mx-auto my-20 rounded-lg shadow-lg sm:max-w-xs md:max-w-sm hover:shadow-xl">
         <img src="https://via.placeholder.com/150" alt="User" 
-        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto
+        hover:scale-110 transition-transform duration-300 ease-in-out"
         />
-        <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+        <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
         <p className="text-gray-600 md:text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
