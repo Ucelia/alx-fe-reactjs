098f98cd78b957ad5baf8488c98938751308f1c4
celia <neevecelia@gmail.com>
2024-12-08 14:42:43 +0200

    Added recipe detail page

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import recipeData from '../data.json'
+import { Link } from 'react-router-dom';
 
 const HomePage =() =>{
   const [recipes, setRecipes] = useState([]);
@@ -12,14 +13,14 @@ const HomePage =() =>{
   return(
     <div>
         <div className="container bg-white mx-auto p-6 rounded-xl shadow-lg">
-  <h1 className="text-3xl text-lime-950 mb-6">Recipe Sharing Platform</h1>
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 rounded-md p-4">
-    {recipes.map((recipe) => (
-      <div
+        <h1 className="text-3xl text-lime-950 mb-6">Recipe Sharing Platform</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 rounded-md p-4">
+            {recipes.map((recipe) => (
+        <div
         key={recipe.id}
         className=" bg-lime-800 rounded-lg mx-auto shadow-md p-4 transform hover:scale-105 
         transition-transform ease-in-out"
-      >
+        >
         <img
           src={recipe.image}
           alt={recipe.title}
@@ -28,9 +29,11 @@ const HomePage =() =>{
         <div className="p-4">
           <h2 className="text-lg font-bold mb-2">{recipe.title}</h2>
           <p className=" mb-4">{recipe.summary}</p>
-          <button className="bg-lime-600 text-white px-4 py-2 rounded hover:bg-lime-700">
-            View Details
-          </button>
+          <Link to={`/recipe/${recipe.id}`}>
+            <button className="bg-lime-600 text-white px-4 py-2 rounded hover:bg-lime-700">
+                View Details
+            </button>
+          </Link>
         </div>
       </div>
     ))}
---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import recipeData from '../data.json';
+import { useParams } from 'react-router-dom';
+
+const RecipeDetail=()=> {
+    const {id} = useParams();
+    const [recipe, setRecipe] = useState(null);
+
+    useEffect(()=>{
+        const selectedRecipe= recipeData.find((recipe)=> recipe.id === parseInt(id));
+        setRecipe(selectedRecipe);
+    },[id]);
+
+    if (!recipe) {
+        return <div>Loading...</div>;
+      }
+
+  return (
+    <div className='container bg-white p-4 mx-auto'>
+        <div className="text-center bg-lime-800 p-4 rounded-md max-w-lg">
+        <img
+          src={recipe.image}
+          alt={recipe.title}
+          className="mx-auto rounded-full"
+        />
+        <h1 className="text-4xl font-bold text-lime-950 italic">{recipe.title}</h1>
+        <p className="text-lime-50 mt-4">{recipe.summary}</p>
+        <div className=''>
+        <p className="text-lime-50 mt-4"><b>Ingredients: </b>{recipe.ingredients}</p>
+        <p className="text-lime-50 text-sm italic font-thin mt-4">{recipe.instructions}</p>
+        </div>
+        
+      </div>
+    </div>
+    
+  )
+}
+
+export default RecipeDetail
