5503ca3b6b2aca0d2618565cb3d5fe6e8644c56e
celia <neevecelia@gmail.com>
2024-12-16 00:17:06 +0200

    changes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com/search/users?q=';
+const GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN;
 
 export const fetchUserData = async ({username, location, minRepos}) => {
     let query ='';
@@ -10,8 +11,13 @@ export const fetchUserData = async ({username, location, minRepos}) => {
     if(location) query += `Location: ${location}`;
     if(minRepos) query += `repos: >= ${minRepos}`;
 
+    const headers = {
+        Authorization: `token ${GITHUB_API_TOKEN}`,
+    };
+
+
     try {
-        const response = await axios.get(`${BASE_URL}${query}`);
+        const response = await axios.get(`${BASE_URL}${query}`, { headers });
         return response.data;
     } catch (error) {
         throw new Error(
