9a0fa21270c10337316261f5cf7ecc71169fa8f6
celia <neevecelia@gmail.com>
2025-01-11 18:04:36 +0200

    added the view movie details, implement dark and light mode

--- movie-database-app/src/components/MovieDetails.jsx
+++ movie-database-app/src/components/MovieDetails.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function MovieDetails() {
+const MovieDetails=({movie, onClose})=> {
   return (
-    <div>MovieDetails</div>
-  )
-}
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
+        <div className="bg-white p-6 rounded-lg shadow-lg max-w-md">
+            <button className="mb-6 text-red-500 rounded-full float-right " onClick={onClose}><strong>X</strong></button>
+            <img src={movie.Poster} alt={movie.Title} className="w-full h-64 object-cover rounded" />
+            <h3 className="text-2xl text-black font-bold mt-4">{movie.Title}</h3>
+            <p className='text-black'><strong>Genre:</strong>{movie.Genre}</p>
+            <p className='text-black'><strong>Plot Summary:</strong>{movie.Plot}</p>
+            <p className='text-black'><strong>Actors:</strong>{movie.Actors}</p>
+            <p className='text-black'><strong>Ratings:</strong>{movie.imdbRating}</p>
+        </div>
+    </div>
+  );
+};
 
 export default MovieDetails
--- movie-database-app/src/components/SearchBar.jsx
+++ movie-database-app/src/components/SearchBar.jsx
@@ -1,6 +1,8 @@
+//search bar
+
 import React, { useState } from 'react'
 
-const SearchBar=({onSearch})=> {
+const SearchBar=({onSearch, theme})=> {
     const [query, setQuery]= useState("");
 
     const handleSubmit = ()=>{
@@ -13,7 +15,11 @@ const SearchBar=({onSearch})=> {
     <div className="flex justify-center my-4">
         <input type="text"
         value={query}
-        onChange={(e) => setQuery(e.target.value)} 
+        onChange={(e) => setQuery(e.target.value)} className={`p-2 rounded-l-md w-3/4 border focus:outline-none ${
+            theme === "light"
+              ? "bg-white text-black border-gray-400"
+              : "bg-gray-800 text-white border-gray-600"
+          }`}
         placeholder='Search for a movie'
         />
         <button onClick={handleSubmit} className="bg-green-800 text-white p-2 ml-4 rounded-r-md">Search</button>
